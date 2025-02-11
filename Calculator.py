6cc8e6bc4cf76aeb966ecccc3e53c72d40002b9a
Alyssa Resh <alyssaresh@icloud.com>
2025-02-11 16:41:32 -0500

    wrote basic mathematical functions - add, subtract, multiply, divide

---
+++ Calculator.py
@@ -0,0 +1,25 @@
+operation = input('''
+Please type in the math operation you would like to complete:
++ for addition
+- for subtraction
+* for multiplication
+/ for division
+''')
+
+number_1 = int(input('Enter your first number: '))
+number_2 = int(input('Enter your second number: '))
+
+if operation == '+':
+    print(number_1 + number_2)
+
+elif operation == '-':
+    print(number_1 - number_2)
+
+elif operation == '*':
+    print(number_1 * number_2)
+
+elif operation == '/':
+    print(number_1 / number_2)
+
+else:
+    print("Error, try again")
---
+++ Console.py
@@ -0,0 +1,12 @@
+# class Console:
+
+#     def defaultDisplay(self):
+#         print ("0")
+
+#     def inputOne(self):
+#         inputOne = input("Enter a number")
+
+#     def inputTwo(self):
+#         inputTwo = input("Enter a number")
+    
+
